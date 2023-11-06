91bb3a46d68e356241c20ee163fcd61e8035c4d9
ashleighrei <146857292+ashleighrei@users.noreply.github.com>
2023-11-06 13:44:55 +0000

    Update streamlit_combine.py

--- streamlit_combine.py
+++ streamlit_combine.py
@@ -1,89 +1,43 @@
 import streamlit as st
 import os
-import pypdf
+import fitz  # PyMuPDF
 from io import BytesIO
 import tempfile
 
-# Custom CSS to set the font, colors, and font-weight
-css = f"""
-<style>
-/* Set the font and font-weight for the title */
-.title {{
-    font-family: 'Poppins', sans-serif;
-    font-weight: 700;
-   
-}}
+# Custom CSS and Streamlit setup
+# ... (same as in your previous code)
 
-/* Set the font, colors, and font-weight for the rest of the app */
-body {{
-    font-family: 'Poppins', sans-serif;
-
-}}
-
-</style>
-"""
-
-st.markdown(css, unsafe_allow_html=True)
-
-st.markdown('<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">', unsafe_allow_html=True)
-
-# Function to combine PDFs
+# Function to combine PDFs using PyMuPDF
 def combine_pdfs(input_files, output_file):
-    # Create a PDF merger object
-    pdf_merger = pypdf.PdfMerger()
-
+    pdf_pages = []
     try:
-        # Iterate through input PDF files and append them to the merger
         for input_file in input_files:
-            pdf_merger.append(input_file)
-        
-        # Write the combined PDF to a BytesIO buffer
+            pdf_document = fitz.open(input_file)
+            pdf_pages.extend(pdf_document)
+
+        # Create a BytesIO buffer to store the combined PDF
         pdf_buffer = BytesIO()
-        pdf_merger.write(pdf_buffer)
-        pdf_buffer.seek(0)  # Reset the buffer position
+        pdf_document = fitz.open(pdf_buffer)
+        pdf_document.insertPDF(pdf_pages)
 
         # Save the combined PDF with the specified output file name and PDF extension
-        with open(output_file, 'wb') as out_pdf:
-            out_pdf.write(pdf_buffer.read())
-        
-        return True
+        pdf_document.save(output_file)
 
+        return True
     except Exception as e:
         st.error(f"An error occurred: {str(e)}")
         return False
 
-# Streamlit app
-st.title('Combine Multiple PDFs App')
+# Streamlit app (remaining code remains the same as in your previous code)
 
-# User prompt for the number of files to upload
-num_files = st.number_input("How many PDFs would you like to upload? Max of 15 PDF files.", min_value=1, max_value=15, value=1, step=1)
-
-# Initialize the input_files list to store the uploaded PDFs
-input_files = []
-
-# File upload widgets for individual PDFs (based on user input)
-for i in range(num_files):
-    file = st.file_uploader(f"Upload PDF {i + 1}", type=["pdf"])
-    if file:
-        input_files.append(file)
-
-# User input for the output PDF file name
-output_file_name = st.text_input("Enter a file name for the combined PDF file (or leave empty for default name)")
-
-# Default output file name
-if not output_file_name:
-    output_file_name = "incognito_noname_combined_file.pdf"
-else:
-    # Ensure that the output file name has a .pdf extension
-    if not output_file_name.lower().endswith(".pdf"):
-        output_file_name += ".pdf"
+# ...
 
 # Display a "Combine" button to combine the PDFs
 if st.button("Combine") and input_files:
     # Output file where the combined PDF will be saved
     output_file_path = os.path.join(tempfile.gettempdir(), output_file_name)
 
-    # Call the function to combine the PDFs
+    # Call the function to combine the PDFs using PyMuPDF
     if combine_pdfs(input_files, output_file_path):
         st.success("PDFs successfully combined.")
         
