29522d7bbbfba924cb8d200ea2d62a17e8c269a1
ashleighrei <146857292+ashleighrei@users.noreply.github.com>
2023-11-06 16:45:32 +0000

    Update streamlit_combine.py

--- streamlit_combine.py
+++ streamlit_combine.py
@@ -36,4 +36,18 @@ def combine_pdfs(input_files, output_file):
 # Display a "Combine" button to combine the PDFs
 if st.button("Combine") and uploaded_files:
     # Output file where the combined PDF will be saved
-    output_file_path = os.path.join(tempfile.gettemp
+    output_file_path = os.path.join(tempfile.gettempdir(), output_file_name)
+
+    # Call the function to combine the PDFs using PyPDF2
+    if combine_pdfs(uploaded_files, output_file_path):  # Use uploaded_files, not input_files
+        st.success("PDFs successfully combined.")
+
+        # Display a "Download" button for the combined PDF
+        with open(output_file_path, "rb") as file:
+            st.download_button(
+                label="Download",
+                data=file.read(),
+                file_name=output_file_name,
+            )
+else:
+    st.warning("Please upload at least one PDF file :)")
