a5063d680343ca328b0824091236b4ed02cf2a8d
ashleighrei <146857292+ashleighrei@users.noreply.github.com>
2023-11-06 14:07:20 +0000

    Update streamlit_combine.py

--- streamlit_combine.py
+++ streamlit_combine.py
@@ -1,53 +1,23 @@
 import streamlit as st
 import os
-import PyPDF2
-import tempfile
+from PyPDF4 import PdfFileReader, PdfFileWriter
 from io import BytesIO
-import base64
+import tempfile
 
-import streamlit as st
+# Custom CSS and Streamlit setup
+# ... (same as in your previous code)
 
-
-# Custom CSS to set the font, colors, and font-weight
-css = f"""
-<style>
-/* Set the font and font-weight for the title */
-.title {{
-    font-family: 'Poppins', sans-serif;
-    font-weight: 700;
-   
-}}
-
-/* Set the font, colors, and font-weight for the rest of the app */
-body {{
-    font-family: 'Poppins', sans-serif;
-
-}}
-
-</style>
-"""
-
-st.markdown(css, unsafe_allow_html=True)
-
-st.markdown('<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">', unsafe_allow_html=True)
-
-# Rest of your Streamlit app code
-# ...
-
-st.markdown('<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">', unsafe_allow_html=True)
-
-
-# Function to combine PDFs
+# Function to combine PDFs using PyPDF4
 def combine_pdfs(input_files, output_file):
-    # Create a PDF merger object
-    pdf_merger = PyPDF2.PdfMerger()
+    pdf_merger = PdfFileWriter()
 
     try:
-        # Iterate through input PDF files and append them to the merger
         for input_file in input_files:
-            pdf_merger.append(input_file)
-        
-        # Write the combined PDF to a BytesIO buffer
+            pdf_reader = PdfFileReader(input_file)
+            for page_num in range(pdf_reader.getNumPages()):
+                pdf_merger.addPage(pdf_reader.getPage(page_num))
+
+        # Create a BytesIO buffer to store the combined PDF
         pdf_buffer = BytesIO()
         pdf_merger.write(pdf_buffer)
         pdf_buffer.seek(0)  # Reset the buffer position
@@ -55,60 +25,25 @@ def combine_pdfs(input_files, output_file):
         # Save the combined PDF with the specified output file name and PDF extension
         with open(output_file, 'wb') as out_pdf:
             out_pdf.write(pdf_buffer.read())
-        
-        return True
 
+        return True
     except Exception as e:
         st.error(f"An error occurred: {str(e)}")
         return False
 
-# Streamlit app
-st.title('Combine Multiple PDFs App')
+# Streamlit app (remaining code remains the same as in your previous code)
 
-# User prompt for the number of files to upload
-num_files = st.number_input("How many PDFs would you like to upload? Max of 15 PDF files.", min_value=1, max_value=15, value=1, step=1)
-
-# File upload widgets for individual PDFs (based on user input)
-uploaded_files = []
-for i in range(num_files):
-    file = st.file_uploader(f"Upload PDF {i + 1}", type=["pdf"])
-    if file:
-        uploaded_files.append(file)
-
-# User input for the output PDF file name
-output_file_name = st.text_input("Enter a file name for the combined PDF file (or leave empty for default name)")
-
-# Default output file name
-if not output_file_name:
-    output_file_name = "incognito_noname_combined_file.pdf"
-else:
-    # Ensure that the output file name has a .pdf extension
-    if not output_file_name.lower().endswith(".pdf"):
-        output_file_name += ".pdf"
+# ...
 
 # Display a "Combine" button to combine the PDFs
-if st.button("Combine") and uploaded_files:
-    # Temporary directory to store uploaded files
-    temp_dir = 'temp'
-    
-    # Create the temporary directory if it doesn't exist
-    os.makedirs(temp_dir, exist_ok=True)
-
-    # Save uploaded files to the temporary directory
-    for i, uploaded_file in enumerate(uploaded_files):
-        with open(os.path.join(temp_dir, f"file_{i + 1}.pdf"), 'wb') as f:
-            f.write(uploaded_file.read())
-
-    # List all PDF files in the temporary directory
-    input_files = [os.path.join(temp_dir, filename) for filename in os.listdir(temp_dir) if filename.endswith(".pdf")]
-
+if st.button("Combine") and input_files:
     # Output file where the combined PDF will be saved
     output_file_path = os.path.join(tempfile.gettempdir(), output_file_name)
 
-    # Call the function to combine the PDFs
+    # Call the function to combine the PDFs using PyPDF4
     if combine_pdfs(input_files, output_file_path):
         st.success("PDFs successfully combined.")
-        
+
         # Display a "Download" button for the combined PDF
         with open(output_file_path, "rb") as file:
             st.download_button(
@@ -116,10 +51,5 @@ if st.button("Combine") and uploaded_files:
                 data=file.read(),
                 file_name=output_file_name,
             )
-
-        # Remove the temporary directory and its contents
-        for file in os.listdir(temp_dir):
-            os.remove(os.path.join(temp_dir, file))
-        os.rmdir(temp_dir)
 else:
     st.warning("Please upload at least one PDF file :)")
